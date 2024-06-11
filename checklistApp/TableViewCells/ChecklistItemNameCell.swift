6a24f3e09aad7f4c8aabf7f115674cc98e899f63
Kuldeep Singh <kponia05@gmail.com>
2024-06-11 17:41:20 -0500

    Completed the Checklist App and added a test api file

---
+++ checklistApp/TableViewCells/ChecklistItemNameCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChecklistItemNameCell.swift
+//  checklistApp
+//
+//  Created by Kuldeep Singh on 6/11/24.
+//
+
+import UIKit
+
+class ChecklistItemNameCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
